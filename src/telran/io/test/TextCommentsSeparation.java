6a781cbd4ec8e8ab9276051865f71673cbb366bb
Daniel Kleyman <denny_klm1@yahoo.com>
2023-09-18 16:06:02 +0300

    HW-32

--- src/telran/io/test/TextCommentsSeparation.java
+++ src/telran/io/test/TextCommentsSeparation.java
@@ -9,27 +9,33 @@ import java.util.regex.Pattern;
 
 public class TextCommentsSeparation {
 
-	public static void main(String[] args) throws IOException {
-		String pattern = "^\\s*//";
-		String[] arguments = { "C:/Users/Daniel/Desktop/test/originalFileWithComments.txt",
-				"C:/Users/Daniel/Desktop/test/onlyComments.txt", "C:/Users/Daniel/Desktop/test/onlyText.txt" };
+    public static void main(String[] args) throws IOException {
+        String pattern = "^\\s*//w*";
+        String[] arguments = {
+            "C:/Users/Daniel/Desktop/test/originalFileWithComments.txt",
+            "C:/Users/Daniel/Desktop/test/onlyComments.txt",
+            "C:/Users/Daniel/Desktop/test/onlyText.txt"
+        };
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(arguments[0]));
-				BufferedWriter writer = new BufferedWriter(new FileWriter(arguments[1]));
-				BufferedWriter writer1 = new BufferedWriter(new FileWriter(arguments[2]))) {
-			reader.lines().forEach(line -> {
-				try {
-					if (Pattern.matches(pattern, line)) {
-						writer.write(line);
-						writer.newLine();
-					} else {
-						writer1.write(line);
-						writer1.newLine();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-		}
-	}
+        try (
+            BufferedReader reader = new BufferedReader(new FileReader(arguments[0]));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(arguments[1]));
+            BufferedWriter writer1 = new BufferedWriter(new FileWriter(arguments[2]))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (Pattern.matches(pattern, line)) {
+                    writer.write(line);
+                    writer.newLine();
+                } else {
+                    writer1.write(line);
+                    writer1.newLine();
+                }
+            }
+        }
+    }
 }
+
+
+
+

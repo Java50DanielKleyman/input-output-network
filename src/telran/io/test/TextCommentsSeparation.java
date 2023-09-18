4694092fced7a54a4cfb89cdd2b2476caf95ed1c
Daniel Kleyman <denny_klm1@yahoo.com>
2023-09-18 15:56:43 +0300

    HW-32

--- src/telran/io/test/TextCommentsSeparation.java
+++ src/telran/io/test/TextCommentsSeparation.java
@@ -6,35 +6,30 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class TextCommentsSeparation {
 
-    public static void main(String[] args) throws IOException {
-        String pattern = "^\\s*//";
-        String[] arguments = {
-            "C:/Users/Daniel/Desktop/test/originalFileWithComments.txt",
-            "C:/Users/Daniel/Desktop/test/onlyComments.txt",
-            "C:/Users/Daniel/Desktop/test/onlyText.txt"
-        };
+	public static void main(String[] args) throws IOException {
+		String pattern = "^\\s*//";
+		String[] arguments = { "C:/Users/Daniel/Desktop/test/originalFileWithComments.txt",
+				"C:/Users/Daniel/Desktop/test/onlyComments.txt", "C:/Users/Daniel/Desktop/test/onlyText.txt" };
 
-        try (
-            BufferedReader reader = new BufferedReader(new FileReader(arguments[0]));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(arguments[1]));
-            BufferedWriter writer1 = new BufferedWriter(new FileWriter(arguments[2]))
-        ) {
-            // Separate comments and non-comments using collect
-            String comments = reader.lines()
-                    .filter(line -> Pattern.matches(pattern, line))
-                    .collect(Collectors.joining(System.lineSeparator()));
-            
-            String text = reader.lines()
-                    .filter(line -> !Pattern.matches(pattern, line))
-                    .collect(Collectors.joining(System.lineSeparator()));
-            
-            // Write the comments and text to their respective files
-            writer.write(comments);
-            writer1.write(text);
-        }
-    }
+		try (BufferedReader reader = new BufferedReader(new FileReader(arguments[0]));
+				BufferedWriter writer = new BufferedWriter(new FileWriter(arguments[1]));
+				BufferedWriter writer1 = new BufferedWriter(new FileWriter(arguments[2]))) {
+			reader.lines().forEach(line -> {
+				try {
+					if (Pattern.matches(pattern, line)) {
+						writer.write(line);
+						writer.newLine();
+					} else {
+						writer1.write(line);
+						writer1.newLine();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+	}
 }

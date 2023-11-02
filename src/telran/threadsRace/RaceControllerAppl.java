8d4b49e6a949e1e44299ae89a58121f20b007457
Daniel Kleyman <denny_klm1@yahoo.com>
2023-11-02 15:44:53 +0200

    HW-41

--- src/telran/threadsRace/RaceControllerAppl.java
+++ src/telran/threadsRace/RaceControllerAppl.java
@@ -3,7 +3,6 @@ package telran.threadsRace;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
-
 public class RaceControllerAppl {
 
 	public static void main(String[] args) {
@@ -27,44 +26,24 @@ public class RaceControllerAppl {
 
 	private static int getWinnerNumber(int racers, Race race, int distance) {
 
-		  Thread[] racersArray = getRacers(racers, race, distance);
-		  for (Thread racer : racersArray) {
-		        racer.start();
-		       }
-		  for (Thread racer : racersArray) {
-		        try {
-					racer.join();
-				} catch (InterruptedException e) {					
-					e.printStackTrace();
-				}
-		       }
-//		    CountDownLatch latch = new CountDownLatch(racers);
-//
-//		    for (Thread racer : racersArray) {
-//		        racer.start();
-//		        new Thread(() -> {
-//		            try {
-//		                racer.join();
-//		                latch.countDown();
-//		            } catch (InterruptedException e) {
-//		                e.printStackTrace();
-//		            }
-//		        }).start();
-//		    }
-//
-//		    try {
-//		        latch.await();
-//		    } catch (InterruptedException e) {
-//		        e.printStackTrace();
-//		    }
-
+		Thread[] racersArray = getRacers(racers, race, distance);
+		for (Thread racer : racersArray) {
+			racer.start();
+		}
+		for (Thread racer : racersArray) {
+			try {
+				racer.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		return race.getWinner();
 	}
 
 	private static Thread[] getRacers(int racers, Race race, int distance) {
 		Thread[] racersArray = new Thread[racers];
 		int sleepingTime = race.getSleepingTime();
-		for(int i = 0; i < racersArray.length; i++) {
+		for (int i = 0; i < racersArray.length; i++) {
 			racersArray[i] = new Racer(race, distance, sleepingTime);
 		}
 		return racersArray;
